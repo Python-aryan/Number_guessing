b0fec29d9933cc87287c1cc7c93f85a67da4601b
Aryan Sonone <75265917+Coding-Aryan@users.noreply.github.com>
2023-10-02 17:01:15 +0530

    Update guessing.py

--- guessing.py
+++ guessing.py
@@ -22,18 +22,18 @@ while count < math.log(upper - lower + 1, 2):
 	count += 1
 
 	# taking guessing number as input
-	guess = int(input("Guess a number:- "))
+	guess = int(input("Guess a number: "))
 
 	# Condition testing
 	if x == guess:
-		print("Congratulations you did it in ",
+		print("Congratulations you guessed it in ",
 			count, " try")
 		# Once guessed, loop will break
 		break
 	elif x > guess:
 		print("You guessed too small!")
 	elif x < guess:
-		print("You Guessed too high!")
+		print("You guessed too high!")
 
 # If Guessing is more than required guesses,
 # shows this output.
